ce2719831cd6d24dc2c3d1f38f91ddaa38939873
mojtabavi <mojtabavi99@yahoo.com>
2020-05-11 22:39:32 +0430

    settings for MEDIA

--- blog_project/config/urls.py
+++ blog_project/config/urls.py
@@ -21,3 +21,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
