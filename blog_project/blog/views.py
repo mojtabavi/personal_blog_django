d37ee1280563f3adbca92e4b76a5ca626ac80945
mojtabavi <mojtabavi99@yahoo.com>
2020-05-13 01:11:18 +0430

    Setting Up templates & static

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -19,7 +19,7 @@ def detail(request, slug):
      
     context = {
         "article" : Article.objects.get(slug=slug)
-        
+
             
     }
-    return render(request, 'blog/detail.html',context)
+    return render(request, 'blog/single.html',context)
