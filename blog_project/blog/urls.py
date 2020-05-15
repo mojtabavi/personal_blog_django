c65ba1417c2731260959283f1d3ed3759b3561fc
mojtabavi <mojtabavi99@yahoo.com>
2020-05-16 00:47:36 +0430

    Create Paginator in articles and category

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -4,7 +4,9 @@ from .views import home, detail, category
 app_name = "blog"
 urlpatterns = [
     path('', home, name="home"),
+    path('p<int:page>', home, name="home"),
     path('article/<slug:slug>', detail, name="detail"),
     path('category/<slug:slug>', category, name="category"),
+    path('category/<slug:slug>/p<int:page>', category, name="category"),
 
 ]
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, JsonResponse, Http404
 from .models import Article, Category
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
 #first page of site
-def home(request):
+def home(request, page=1):
 
-    articles = Article.objects.published().order_by('-publish')
+    article_list = Article.objects.published().order_by('-publish')
+    paginator = Paginator(article_list, 3)
+
+    try:
+        articles = paginator.page(page)
+    except PageNotAnInteger:
+        articles = paginator.page(1)
+    except EmptyPage:
+        articles = paginator.page(paginator.num_pages)
     
     context = {
         "articles" : articles,
@@ -24,8 +33,20 @@ def detail(request, slug):
     return render(request, 'blog/detail.html',context)
 
 
-def category(request, slug):
+def category(request, slug, page=1):
+    category_obj = get_object_or_404(Category, slug=slug, status=True)
+    category_list = category_obj.articles.published()
+    paginator = Paginator(category_list, 3)
+
+    try:
+        articles_cg = paginator.page(page)
+    except PageNotAnInteger:
+        articles_cg = paginator.page(1)
+    except EmptyPage:
+        articles_cg = paginator.page(paginator.num_pages)
+    
     context = {
-        "category" : get_object_or_404(Category, slug=slug, status=True)            
+        "category" : category_obj,
+        "articles" : articles_cg,            
     }
     return render(request, 'blog/category.html',context)
