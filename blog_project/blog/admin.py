80ac6690e9eb99278f695e5bc6eb4578db981fb3
mojtabavi <mojtabavi99@yahoo.com>
2020-05-17 00:54:08 +0430

    Actions in AdminPanel Created For Articles

--- blog_project/blog/admin.py
+++ blog_project/blog/admin.py
@@ -4,6 +4,27 @@ from .models import Article,Category,TitleSettings
 # Register your models here.
 
 
+# ساخت اکشن برای پنل ادمین برای منتشر ساختن مقاله ها
+def make_published(self, request, queryset):
+    rows_updated = queryset.update(status = 'p')
+    if rows_updated == 1:
+        message_bit = "یک مقاله به حالت انتشار در آمد"
+    else:
+        message_bit = "%s مقاله به حالت انتشار در آمد" % rows_updated
+    self.message_user(request, message_bit)
+make_published.short_description = "انتشار مقاله های انتخاب شده"
+
+
+# ساخت اکشن برای پنل ادمین برای پیش نویس کردن مقاله ها
+def make_draft(self, request, queryset):
+    rows_updated = queryset.update(status = 'd')
+    if rows_updated == 1:
+        message_bit = "یک مقاله به حالت پیش نویس در آمد"
+    else:
+        message_bit = "%s مقاله به حالت پیش نویس در آمد" % rows_updated
+    self.message_user(request, message_bit)
+make_draft.short_description = "عدم انتشار مقاله های انتخاب شده"
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('position', 'title','parent', 'slug', 'status')
     list_filter = (['status'])
@@ -20,6 +41,7 @@ class ArticleAdmin(admin.ModelAdmin):
     search_fields = ('title', 'description')
     prepopulated_fields = {'slug':('title',)}
     ordering = ['status', '-publish']
+    actions = [make_published,make_draft]
 
     def category_to_str(self, obj):
         return ", ".join([category.title for category in obj.category_published()])
