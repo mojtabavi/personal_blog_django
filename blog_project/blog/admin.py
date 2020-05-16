e8c5214a2a0ad52a019c6c65c5225f16a2e22fb1
mojtabavi <mojtabavi99@yahoo.com>
2020-05-17 02:33:01 +0430

    Change Font of AdminPanel

--- blog_project/blog/admin.py
+++ blog_project/blog/admin.py
@@ -36,7 +36,7 @@ admin.site.register(Category, CategoryAdmin)
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'jpublish', 'status', 'category_to_str')
+    list_display = ('title','thumbnail_tag' ,'slug', 'jpublish', 'status', 'category_to_str')
     list_filter = ('publish', 'status')
     search_fields = ('title', 'description')
     prepopulated_fields = {'slug':('title',)}
--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.html import format_html
 from django.utils import timezone
 from extensions.utils import jalali_convertor
 
@@ -50,6 +51,12 @@ class Article(models.Model):
 
     def category_published(self):
         return self.category.filter(status=True)
+
+    #نمایش عکس های بند انگشتی در پنل ادمین
+    def thumbnail_tag(self):
+        return format_html("<img width=120 src='{}' style='border-radius: 5px;'>".format(self.thumbnail.url))
+    thumbnail_tag.short_description ="عکس بند انگشتی"
+
 
     objects = ArticleManager()
 
